a1c25f9c755b5143860e51c3d531c42fd741e01f
Anthony <8753187+bigfoott@users.noreply.github.com>
2025-07-08 13:34:39 -0400

    Update scrape.js

--- scrape.js
+++ scrape.js
@@ -16,6 +16,12 @@ function main()
         JSDOM.fromURL("https://www.merriam-webster.com/word-of-the-day/calendar", {
         })
         .then((dom) => {
+            console.log("1 " + dom);
+            console.log("2 " + dom.window);
+            console.log("3 " + dom.window.document);
+            console.log("4 " + dom.window.document.documentElement);
+            console.log("5 " + dom.window.document.documentElement.innerHTML);
+            
             var container = dom.window.document.getElementsByClassName("slick-current")[0];
 
             var word = container.getElementsByClassName("wod-l-hover")[0].innerText;

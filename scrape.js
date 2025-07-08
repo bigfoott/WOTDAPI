cced567e9628cba7b1036c1c38c3b58c1eb41e23
Anthony <8753187+bigfoott@users.noreply.github.com>
2025-07-08 10:13:15 -0400

    Update scrape.js

--- scrape.js
+++ scrape.js
@@ -2,7 +2,27 @@ const fs = require('fs');
 
 function main()
 {
-    
+    return new Promise(resolve => {
+        JSDOM.fromURL("https://www.merriam-webster.com/word-of-the-day/calendar", {
+        })
+        .then((dom) => {
+
+            var container = dom.window.document.getElementById("slick-slide00");
+
+            var word = container.getElementsByClassName("wod-l-hover")[0].innerText;
+            var def = container.getElementsByClassName("definition-block").innerText;
+
+            fs.writeFile('files/word.json', '{"word": "' + word + '", "def": "' + def + '"}', err => {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+            });
+        }).catch(_err =>
+            {
+                console.log(_err);
+            });
+    })
 }
 
 try
